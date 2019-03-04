55834fdcc9114335c7c195b54eb2d1858c68aa2f
Игорь Филин <geekfil@gmail.com>
2019-03-04 03:36:51 +0300

    update

--- now/handler.go
+++ now/handler.go
@@ -3,15 +3,21 @@ package main
 import (
 	"github.com/geekfil/zoom-api-service/app"
 	"github.com/geekfil/zoom-api-service/di"
+	"github.com/labstack/echo"
 	"log"
 	"net/http"
 )
 
-func Handler(w http.ResponseWriter, r *http.Request) {
+var server *echo.Echo
+
+func init() {
 	err := di.Container.Invoke(func(app *app.App) {
-		app.Echo.ServeHTTP(w, r)
+		server = app.Echo
 	})
 	if err != nil {
 		log.Panicln(err)
 	}
+}
+func Handler(w http.ResponseWriter, r *http.Request) {
+	server.ServeHTTP(w, r)
 }
